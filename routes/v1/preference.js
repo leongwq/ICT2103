8fdca75c175f492ececdec816fd39e8b5cccb6b1
Leong Wen Qing <leongwenqing@tech.gov.sg>
2018-11-05 20:36:37 +0800

    Added the deletion of preference as well as changing the date of trip in preference.

--- routes/v1/preference.js
+++ routes/v1/preference.js
@@ -4,5 +4,7 @@ const auth = require('../../utils/jwtAuthenticator');
 
 router.post('/', auth, preference.addPreference); // Add new preference
 router.get('/preference/:id', auth, preference.getPreferenceByID); // Get preference by ID
+router.put('/changedate', auth, preference.changeDateByID); // Modify preference date
+router.delete('/preference/:id', auth, preference.deletePreferenceByID); // Delete preference by ID
 
 module.exports = router;
--- services/preference.js
+++ services/preference.js
@@ -29,4 +29,29 @@ preferenceService.getPreferenceByID = async (req, res) => {
     }
 }
 
+preferenceService.changeDateByID = async (req, res) => {
+    let conn;
+    try {
+        conn = await db.getConnection();
+        const data = await conn.query("UPDATE trip_preference SET date = (?) WHERE id = (?) AND userid = (?)", [req.body.date, req.body.id, req.payload._id]);
+            res.status(200).send(data);
+    } catch (err) {
+        res.status(409).send(err);
+    } finally {
+        if (conn) return conn.end();
+    }
+}
+
+preferenceService.deletePreferenceByID = async (req, res) => {
+    let conn;
+    try {
+        conn = await db.getConnection();
+        const data = await conn.query("DELETE FROM trip_preference WHERE id = (?) AND userid = (?)", [req.params.id, req.payload._id]);
+            res.status(200).send(data);
+    } catch (err) {
+        res.status(409).send(err);
+    } finally {
+        if (conn) return conn.end();
+    }
+}
 module.exports = preferenceService;
